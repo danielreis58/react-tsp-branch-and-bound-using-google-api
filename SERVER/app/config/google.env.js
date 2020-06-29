943b054e7e18809096dc3baca697482b4ab73566
Daniel Reis <danielpreis@uol.com.br>
2020-06-28 22:04:27 -0300

    changes

--- SERVER/app/config/google.env.js
+++
@@ -1,8 +0,0 @@
-const env = {
-	AWS_ACCESS_KEY: 'AKIA2BPCE2YWGGCD3BXU',
-	AWS_SECRET_ACCESS_KEY: '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',
-	REGION: 'sa-east-1',
-	Bucket: 'dengueimages'
-};
-
-module.exports = env;
